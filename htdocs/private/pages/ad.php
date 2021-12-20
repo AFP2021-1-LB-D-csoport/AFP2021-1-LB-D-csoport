0d1f337d2ea3b73312912108571d6d134a654e30
safarcsikViktor <85190682+safarcsikViktor@users.noreply.github.com>
2021-12-20 10:07:34 +0100

    Create ad.php

---
+++ htdocs/private/pages/ad.php
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+<form action="" method="post" class="col-lg-3 col-md-4 col-sm-6 mx-auto">
+  <fieldset>
+    <legend class="display-6 mb-4">Hirdetés feladása</legend>
+    <br>
+    <div class="form-group h5">
+      <label class="p-2" for="település">*Település:</label><br>
+      <input class="form-control" type="text" id="település" name="település" required>
+    </div>
+    <div class="form-group h5">
+      <label class="p-2" for="utca">*Utca:</label><br>
+      <input class="form-control mb-3" type="text" id="utca" name="utca" required>
+    </div>
+    <div class="form-group h5">
+      <label class="p-2" for="házszám">*Házszám:</label><br>
+      <input class="form-control" type="text" id="házszám" name="házszám" required>
+    </div>
+    <div class="form-group h5">
+      <label class="p-2" for="hszám">*Helyrajzi szám:</label><br>
+      <input class="form-control" type="text" id="hszám" name="hszám">
+    </div>
+  </fieldset>
+  </br>
+  <input class="btn btn-primary btn-md h5" name="submit" id="submit" type="submit" value="Küldés">
+</form>
+=======
+<!DOCTYPE html>
+<html>
+
+    <head></head>
+        <body>
+
+        <form action ="" method="post">
+           Telepules: <input type = "text" name="varos"/><br/> <br/>
+           Utca <input type = "text" name="hazszam"/> <br/> <br/>
+           Hazszam <input type = "text" name="utca"/><br/> <br/>
+           Helyrajzi szam <input type = "text" name="utca"/><br/> <br/>
+            <input type = "submit" value="Kuldd!"/>
+        </form>
+        </body>
+
+</html>
+<?php
+?>
+>>>>>>> a717f7a88cf3a10957f971debda52f75bc46b994
