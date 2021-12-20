1e6dcd827f719d126387b4cc5c14c30c4a734324
teregudi <teregudi.tamashi@gmail.com>
2021-12-20 10:47:31 +0100

    kereső backend + kilistázás frontend

--- htdocs/private/utils/require.php
+++ htdocs/private/utils/require.php
@@ -11,7 +11,7 @@ define('BASE_URL', 'http://localhost/AFP2021-1-LB-D-csoport/htdocs/index.php');
 define('IMG_DIR', './public/img/');
 
 define('DB_TYPE', 'mysql');
-define('DB_NAME', 'database');
+define('DB_NAME', 'real_estate');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'dbuser');
-define('DB_PASS', 'passwd');
+define('DB_USER', 'root');
+define('DB_PASS', '');
