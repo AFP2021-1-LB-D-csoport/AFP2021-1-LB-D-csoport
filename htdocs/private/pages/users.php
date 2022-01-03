52221b1a59791da56ebb80ec1367b4b126d01a03
teregudi <teregudi.tamashi@gmail.com>
2022-01-03 11:43:38 +0100

    user profile settings backend

--- htdocs/private/pages/users.php
+++ htdocs/private/pages/users.php
@@ -57,7 +57,17 @@ function edit_name() {
     if (!filter_has_var(INPUT_POST, 'edit_name')){
         require USERS_DIR.'edit_name.php';
     } else {
-        
+        $new_name = filter_input(INPUT_POST, 'username');
+        $id = $_SESSION['user']['profile_id'];
+        $query = 'UPDATE profile SET user_name = :new_name WHERE profile_id = :id';
+        $params = ['new_name' => $new_name, 'id' => $id];
+        $success = update($query, $params);
+        if ($success) {
+            $_SESSION['user']['user_name'] = $new_name;
+            header('Location: '.BASE_URL);
+        } else {
+            echo 'Sikertelen módosítás.';
+        }
     }
 }
 
@@ -65,7 +75,17 @@ function edit_email() {
     if (!filter_has_var(INPUT_POST, 'edit_email')){
         require USERS_DIR.'edit_email.php';
     } else {
-        
+        $new_email = filter_input(INPUT_POST, 'email');
+        $id = $_SESSION['user']['profile_id'];
+        $query = 'UPDATE profile SET email_address = :new_email WHERE profile_id = :id';
+        $params = ['new_email' => $new_email, 'id' => $id];
+        $success = update($query, $params);
+        if ($success) {
+            $_SESSION['user']['email_address'] = $new_email;
+            header('Location: '.BASE_URL);
+        } else {
+            echo 'Sikertelen módosítás.';
+        }
     }
 }
 
@@ -73,7 +93,17 @@ function edit_phone() {
     if (!filter_has_var(INPUT_POST, 'edit_phone')){
         require USERS_DIR.'edit_phone.php';
     } else {
-        
+        $new_phone = filter_input(INPUT_POST, 'phone');
+        $id = $_SESSION['user']['profile_id'];
+        $query = 'UPDATE profile SET phone_number = :new_phone WHERE profile_id = :id';
+        $params = ['new_phone' => $new_phone, 'id' => $id];
+        $success = update($query, $params);
+        if ($success) {
+            $_SESSION['user']['phone_number'] = $new_phone;
+            header('Location: '.BASE_URL);
+        } else {
+            echo 'Sikertelen módosítás.';
+        }
     }
 }
 
@@ -81,6 +111,22 @@ function change_password() {
     if (!filter_has_var(INPUT_POST, 'change_password')){
         require USERS_DIR.'change_password.php';
     } else {
-        
+        $old_pwd = filter_input(INPUT_POST, 'old_pwd');
+        $new_pwd = filter_input(INPUT_POST, 'pwd');
+        $new_pwd_confirm = filter_input(INPUT_POST, 'pwd_confirm');
+        $id = $_SESSION['user']['profile_id'];
+        if ($old_pwd !== $_SESSION['user']['pwd'] || $new_pwd !== $new_pwd_confirm) {
+            echo 'Helytelenül megadott adatok.';
+            return;
+        }
+        $query = 'UPDATE profile SET pwd = :new_pwd WHERE profile_id = :id';
+        $params = ['new_pwd' => $new_pwd, 'id' => $id];
+        $success = update($query, $params);
+        if ($success) {
+            $_SESSION['user']['pwd'] = $new_pwd;
+            header('Location: '.BASE_URL);
+        } else {
+            echo 'Sikertelen módosítás.';
+        }
     }
 }
